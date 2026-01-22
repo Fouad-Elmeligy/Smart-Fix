243cc19585fcc74f37ea6adf6e964810849063fc
AdelTamer <adeltammer@gmail.com>
2026-01-22 17:57:40 +0200

    add Cairo font

---
+++ app/src/main/java/com/fcitu/smartfix/ui/theme/Font.kt
@@ -0,0 +1,12 @@
+package com.fcitu.smartfix.ui.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import com.fcitu.smartfix.R
+
+val Cairo = FontFamily(
+    Font(R.font.cairo_regular, FontWeight.Normal),
+    Font(R.font.cairo_semibold, FontWeight.SemiBold),
+    Font(R.font.cairo_bold, FontWeight.Bold)
+)
